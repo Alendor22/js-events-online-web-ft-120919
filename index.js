2685d7c90573e7e5a9031e37da5c895d06ba6c17
howardbdev <howarddevennish@gmail.com>
2020-03-27 12:33:06 -0400

    events part two

--- index.js
+++ index.js
@@ -3,11 +3,20 @@ document.addEventListener("DOMContentLoaded", init)
 function init() {
   // target the form
   const newJokeForm = document.getElementById("new-joke-form")
-  const button1 = document.getElementById("button-1")
+  // const buttonRed = document.getElementById("button-red")
+  // const buttonBlue = document.getElementById("button-blue")
+  // const buttonGreen = document.getElementById("button-green")
   // now what?
   // add a listener for the form to submit!
   newJokeForm.addEventListener("submit", addJokeToDOM)
-  button1.addEventListener("click", addJokeToDOM)
+  // buttonRed.addEventListener("click", changeHeaderColor)
+  // buttonBlue.addEventListener("click", changeHeaderColor)
+  // buttonGreen.addEventListener("click", changeHeaderColor)
+
+  const buttonDiv = document.getElementById("button-div")
+  buttonDiv.addEventListener("click", changeHeaderColor)
+
+
 }
 
 function addJokeToDOM(event) {
@@ -23,6 +32,30 @@ function addJokeToDOM(event) {
   contentInput.value = ""
 }
 
+function changeHeaderColor(event) {
+  console.log("CLICKED!")
+  // find the header
+  // change the style of the header
+  const header = document.getElementById("main-header")
+  let newColor = "black"
+  const id = event.target.id
+  // debugger
+  switch (id) {
+    case "button-red":
+      newColor = "red"
+      break
+    case "button-blue":
+      newColor = "blue"
+      break
+    case "button-green":
+      newColor = "green"
+      break
+    default:
+      newColor = "yellow"
+    }
+
+    header.style.color = newColor
+}
 
 // when a user fills out the form, add the joke to the page
 // when we mouseover, make a click happen
